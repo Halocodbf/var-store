90583e647303057379af31e4fe16201adb9ae7a2
DMGravina <dmg.logra@gmail.com>
2025-10-14 17:28:01 -0300

    Atualização de historicos e movimentações

--- movimentacao.py
+++ movimentacao.py
@@ -1,3 +1,7 @@
+import datetime
+
+historico = []
+
 def registrar_movimento(nome, quantidade, tipo):
     """Registra um movimento no histórico."""
     data = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -8,20 +12,20 @@ def registrar_movimento(nome, quantidade, tipo):
         "tipo": tipo
     })
 
-def relatorio_estoque():
+def relatorio_estoque(estoque):
     """Exibe o relatório atual do estoque."""
     print("\n=== RELATÓRIO DE ESTOQUE ===")
-    if not estoque:
+    if not estoque.produtos:
         print("Nenhum produto no estoque.")
     else:
-        for produto, qtd in estoque.items():
-            print(f"{produto}: {qtd} unidade(s)")
+        for produto, obj in estoque.produtos.items():
+            print(f"{obj.nome_produto}: {obj.quantidade} unidade(s)")
 
 def historico_movimentos():
     """Exibe o histórico de todas as movimentações."""
-    print("\n===  HISTÓRICO DE MOVIMENTOS ===")
+    print("\n=== HISTÓRICO DE MOVIMENTOS ===")
     if not historico:
         print("Nenhum movimento registrado.")
     else:
         for mov in historico:
-            print(f"[{mov['data']}] {mov['tipo']} - {mov['produto']} ({mov['quantidade']} unidade(s))")
+            print(f"[{mov['data']}] {mov['tipo']} - {mov['produto']} ({mov['quantidade']} un)")
